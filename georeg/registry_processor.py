3f2297c5b97af5257fcc17d6456eba7291c5afd5
alee35 <ashley_lee@brown.edu>
2015-10-08 12:39:31 -0400

    deleted line 300: unnecessary conversion to grayscale

--- georeg/registry_processor.py
+++ georeg/registry_processor.py
@@ -297,11 +297,10 @@ class RegistryProcessor:
         """performs a close operation on self._image then extracts the contours"""
 
         if make_new_thresh: # if thresh_value is provided then we make a new thresh image
-            rgb = cv2.cvtColor(self._image,cv2.COLOR_GRAY2RGB) # greyscale to color
             if not self.assume_pre_processed:
-                _,self._thresh = cv2.threshold(rgb,self.thresh_value,255,cv2.THRESH_BINARY_INV) # threshold
+                _,self._thresh = cv2.threshold(self._image,self.thresh_value,255,cv2.THRESH_BINARY_INV) # threshold
             else:
-                _,self._thresh = cv2.threshold(rgb,0,255,cv2.THRESH_BINARY_INV) # threshold with 0 threshold value
+                _,self._thresh = cv2.threshold(self._image,0,255,cv2.THRESH_BINARY_INV) # threshold with 0 threshold value
 
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT,kernel_shape)
         closed = cv2.morphologyEx(self._thresh,cv2.MORPH_CLOSE,kernel,iterations = iter) # close
