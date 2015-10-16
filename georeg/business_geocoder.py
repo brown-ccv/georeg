c3d51c8c26e8a564e1e9317392442831889f93f1
Dave Berenbaum <david_berenbaum@brown.edu>
2015-10-16 16:24:35 -0400

    adds brown_geopy submodule for geocoding

--- georeg/business_geocoder.py
+++ georeg/business_geocoder.py
@@ -1,17 +1,19 @@
 import os
 
-import brownarcgis
+from brown_geopy.brownarcgis import BrownArcGIS
 
-geolocator = brownarcgis.BrownArcGIS(username = os.environ.get("BROWNGIS_USERNAME"),
-                                     password = os.environ.get("BROWNGIS_PASSWORD"),
-                                     referer = os.environ.get("BROWNGIS_REFERER"))
+
+geolocator = BrownArcGIS(username = os.environ.get("BROWNGIS_USERNAME"),
+                         password = os.environ.get("BROWNGIS_PASSWORD"),
+                         referer = os.environ.get("BROWNGIS_REFERER"))
+
 
 def geocode_business(business, state = 'RI'):
     """geocode a business object and store the results inside it,
     return confidence score"""
 
     location = geolocator.geocode(street=business.address, city=business.city,
-            state=business.state, zip_cd=business.zip, n_matches = 1, timeout = 10)
+            state=state, zip_cd=business.zip, n_matches = 1, timeout = 10)
 
     if location:
         match = location[0]
@@ -19,4 +21,6 @@ def geocode_business(business, state = 'RI'):
         business.lat = match[2]
         business.long = match[3]
 
-    else: print "unsuccessful geo-query"
+    else: 
+        print "Unsuccessful geo-query: %s, %s, %s, %s" % (business.address,
+                business.city, state, business.zip)
