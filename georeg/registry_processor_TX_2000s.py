39a4b1e97aa6234ae3f121e92ab479f8db2298fa
Dave Berenbaum <david_berenbaum@brown.edu>
2015-11-24 08:47:23 -0500

    updates to TX 2000s reg processor

--- georeg/registry_processor_TX_2000s.py
+++ georeg/registry_processor_TX_2000s.py
@@ -1,11 +1,9 @@
 import registry_processor as reg
 import registry_processor_new as regnew
 import re
-import business_geocode as geo
+import business_geocoder as geo
 
-class RegistryProcessorNewTX(regnew.RegistryProcessorNew):
-
-    current_city = ""
+class RegistryProcessorTX2000s(regnew.RegistryProcessorNew):
 
     # regex patterns to parse blocks
     city_pattern = re.compile(r'{A-Za-z\s]+\n\(.*\)')
@@ -14,21 +12,8 @@ class RegistryProcessorNewTX(regnew.RegistryProcessorNew):
     naics_pattern = re.compile(r'NAICS-([/d]{6}\;)+')
     emp_pattern = re.compile(r'Employs-([/d]+),')
     sales_pattern = re.compile(r'Sales-(.*)')
-
-    def _process_contour(self, contour_txt):
-        registry_match = self.registry_pattern.match(contour_txt)
-        city_match = self.city_pattern.match(contour_txt)
-
-        if registry_match:
-            business = self._parse_registry_block(contour_txt)
-            business.city = self.current_city
-
-            if business.address:
-                geo.geocode_business(business, 'TX')
-
-            self.business.append(business)
-        elif city_match:
-            self.current_city = city_match.group(1)
+    address_pattern = re.compile(r'(.*)\((.*)\)')
+    cat_desc_pattern = re.compile(r'[/d]{6}\;(.*)')
 
     def _parse_registry_block(self, registry_txt):
         """works for registries from 2000-2010"""
@@ -48,7 +33,6 @@ class RegistryProcessorNewTX(regnew.RegistryProcessorNew):
                     break
                 full_address += line
 
-        address_pattern = re.compile(r'(.*)\((.*)\)')
         match = self.address_pattern.search(full_address)
         if match:
             business.address = match.group(1)
@@ -63,24 +47,23 @@ class RegistryProcessorNewTX(regnew.RegistryProcessorNew):
                     break
                 cat_desc += line
 
-        cat_desc_pattern = re.compile(r'[/d]{6}\;(.*)')
         match = self.cat_desc_pattern.search(cat_desc)
         if match:
             business.cat_desc = match.group(1)
 
-        match = self.sic_pattern.search(registry_txt):
+        match = self.sic_pattern.search(registry_txt)
         if match:
             business.category = match.group(1)
 
-        match = self.naics_pattern.search(registry_txt):
+        match = self.naics_pattern.search(registry_txt)
         if match:
             business.new_cat = match.group(1)
     
-        match = self.emp_pattern.search(registry_txt):
+        match = self.emp_pattern.search(registry_txt)
         if match:
             business.emp = match.group(1)
 
-        match = self.sales_pattern.search(registry_txt):
+        match = self.sales_pattern.search(registry_txt)
         if match:
             business.sales = match.group(1)
 
